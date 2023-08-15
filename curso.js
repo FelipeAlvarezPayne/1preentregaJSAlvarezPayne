ae2748b08369b4a38f445eee9bf9b7b01decf30b
Felipe Alvarez Payne <felipe.alvarez.payne@gmail.com>
2023-08-14 22:39:14 -0300

    Entrega

--- curso.js
+++ curso.js
@@ -1,10 +1,11 @@
 function generarNumeroAleatorio(){
+
     // Se le comenta la usuario lo que vamos a realizar
-    alert('Consigna: Elegí 2 números y te indicaré algun numero dentro del rango de ambos');
+    alert('Consigna: Elegí 2 números y te mencionare algún número dentro del rango de ambos');
     // Se le pide al usuario el primer número
-    const numeroInicial = parseInt(prompt('Ingrese el primer número'));
+    const numeroInicial = parseInt(prompt('Ingrese el número inicial'));
     // Se le pide al usuario el segundo número
-    const numeroFinal = parseInt(prompt('Ingrese el segundo número'));
+    const numeroFinal = parseInt(prompt('Ingrese el número final'));
 
     // Se coteja que este ingresando números válidos y que el primero sea mayor que el segundo
     if (isNaN(numeroInicial ) || isNaN(numeroFinal)) {
@@ -12,14 +13,14 @@ function generarNumeroAleatorio(){
         return;
     } else if (numeroFinal < numeroInicial ) {
         alert('El primer número debe ser mayor que el segundo')
-        return
+        return;
     }
 
     //Se genera le número aleatorio con los números ingresados
     numeroAleatorio = Math.floor(Math.random() * (numeroFinal - numeroInicial ) + numeroInicial );
     alert('El número generado es ' + numeroAleatorio);
 
-    //Esto lo generé para testear que los números generados incluyan los números ingresados por el usuario
+    //Aclaro que los números generados incluyan los números ingresados por el usuario
     for (let i = numeroInicial ; i <= numeroFinal; i++) {
         console.log(i);
     }
@@ -27,3 +28,4 @@ function generarNumeroAleatorio(){
 }
 
 generarNumeroAleatorio();
+
